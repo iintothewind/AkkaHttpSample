d6b062f5ae872e5b1b5551e8d35e155cc4ca8d8a
iintothewind <iintothewind@yeah.net>
2018-05-10 14:32:20 +0800

    application.conf: added logger-startup-timeout = 30s FlowBasedRequestLevelClient: change api to mtime.cn

--- src/main/scala/ivar/http/client/FlowBasedRequestLevelClient.scala
+++ src/main/scala/ivar/http/client/FlowBasedRequestLevelClient.scala
@@ -2,6 +2,7 @@ package ivar.http
 package client
 
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model._
 import akka.stream.scaladsl.Source
 import ivar.http.serdes.ZhihuSerdes
@@ -12,7 +13,9 @@ import scala.util.{Failure, Success}
 object FlowBasedRequestLevelClient extends App with ZhihuSerdes {
   val flow = Http().superPool[Unit](settings = connectionPoolSettings)
   Source
-    .single((HttpRequest(uri = "https://news-at.zhihu.com/api/4/news/latest"), ()))
+    .single(
+      (HttpRequest(uri = Uri("https://api-m.mtime.cn/PageSubArea/HotPlayMovies.api")
+        .withQuery(Query("locationId" -> "292"))), ()))
     .via(flow)
     .runForeach {
       case (Success(resp), _) => resp.entity.dataBytes.map(_.utf8String).runForeach(println(_))
